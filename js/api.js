c196d4a4c1300adaf0e8dadb4fad87c0ecfa012a
lxmd4 <lxmd444cloud@gmai.com>
2025-10-08 18:08:12 +0900

    1008

--- js/api.js
+++ js/api.js
@@ -72,7 +72,7 @@ async function uploadImageToS3() {
     
     const reader = new FileReader();
     reader.onload = async (e) => {
-        const base64Image = e.currentTarget.result;
+        const base64Image = e.currentTarget.result.toString('base64');
         
         try {
             document.getElementById('result').innerHTML = '<p>アップロード中...</p>';
